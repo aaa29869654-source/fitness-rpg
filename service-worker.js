9c019f18c45a839463c72254426b13ea0a61a160
aaa29869654-source <aaa29869654@gmail.com>
2026-07-18 07:07:07 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,5 @@
-const CACHE="fitness-odyssey-v7-gp-build1";
+const CACHE="fitness-odyssey-v7-gp-build2";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./service-worker.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(
-    caches.match(e.request).then(cached=>{
-      if(cached) return cached;
-      return fetch(e.request).then(resp=>{
-        const copy=resp.clone();
-        caches.open(CACHE).then(c=>c.put(e.request,copy));
-        return resp;
-      }).catch(()=>caches.match("./index.html"));
-    })
-  );
-});
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match("./index.html"))))});
